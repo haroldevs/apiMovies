fe57d9cefca905917a4dbcd53b7c5107a9713985
haroldevs <haroldthomasg18@gmail.com>
2022-10-08 06:28:21 -0500

    card

---
+++ app.js
@@ -0,0 +1,37 @@
+const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '2511b98d4cmsh0bf687c4a1d0b51p1d49fdjsn86b7328f082d',
+      'X-RapidAPI-Host': 'movies-app1.p.rapidapi.com'
+    }
+  };
+
+const container=document.querySelector(".container")
+//const inputBuscar=document.querySelector(".input-buscar")
+//const btnBuscar=document.querySelector(".btn-buscar")
+
+
+const getMovies=async function(){
+    const respuesta=await fetch("https://movies-app1.p.rapidapi.com/api/movies",options)
+    const data= await respuesta.json()
+
+    listMovies(data.results)
+    //console.log(data)
+}
+
+getMovies()
+
+function listMovies(Movies){
+    container.innerHTML="";
+    Movies.forEach((movie) => {
+        container.innerHTML+=`<div class="card">
+        <img src="${movie.image}" alt="">
+        <h2>${movie.titleOriginal}</h2>
+        <h4>${movie.rating} </h4>
+        <p>
+            ${movie.description}
+        </p>
+        </div>  
+        `
+    });
+}
